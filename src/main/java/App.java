734c826747f156e71c92b8621219a3e5b7b0c76b
Gloria Friesen <gloriafriesen@gmail.com>
2017-04-07 10:28:25 -0700

    add report sighting form and adjust styling on endangered-animal template

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -23,15 +23,14 @@ public class App {
     post("/endangered_sighting", (request, response) -> {
       Map<String, Object> model = new HashMap<String, Object>();
       String rangerName = request.queryParams("rangerName");
-      int animalIdSelected = Integer.parseInt(request.queryParams("endangeredAnimalSelected"));
+      EndangeredAnimal endangeredAnimal = EndangeredAnimal.find(Integer.parseInt(request.queryParams("endangeredAnimalSelected")));
       String latLong = request.queryParams("latLong");
-      Sighting sighting = new Sighting(animalIdSelected, latLong, rangerName);
+      Sighting sighting = new Sighting(endangeredAnimal.getId(), latLong, rangerName);
       sighting.save();
       model.put("sighting", sighting);
       model.put("animals", EndangeredAnimal.all());
-      String animal = EndangeredAnimal.find(animalIdSelected).getName();
-      model.put("animal", animal);
-      model.put("template", "templates/success.vtl");
+      model.put("endangeredAnimal", endangeredAnimal);
+      model.put("template", "templates/endangered_animal.vtl");
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
 
